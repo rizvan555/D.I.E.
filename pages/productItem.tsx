dcc435404e2611b3df998aa4cea81390cac4a0e9
RizvanKarimov <karimovrizvan84@gmail.com>
2023-06-24 22:45:01 +0200

    created new Item Page

---
+++ pages/productItem.tsx
@@ -0,0 +1,112 @@
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+interface Category {
+  category: {
+    drink: {
+      name: string;
+      models: [
+        {
+          name: string;
+          alcohol: string;
+          allergic: {
+            Gluten: string;
+            Milk: string;
+            Egg: string;
+            Nuts: string;
+            Sesame: string;
+            Wheat: string;
+            Fish: string;
+          };
+          halal: string;
+          vegan: string;
+          vegetarian: string;
+        }
+      ];
+    }[];
+    food: {
+      name: string;
+      models: [
+        {
+          name: string;
+          alcohol: string;
+          allergic: {
+            Gluten: string;
+            Milk: string;
+            Egg: string;
+            Nuts: string;
+            Sesame: string;
+            Wheat: string;
+            Fish: string;
+          };
+          halal: string;
+          vegan: string;
+          vegetarian: string;
+        }
+      ];
+    }[];
+    medicine: {
+      name: string;
+      models: [
+        {
+          name: string;
+          alcohol: string;
+          allergic: {
+            Gluten: string;
+            Milk: string;
+            Egg: string;
+            Nuts: string;
+            Sesame: string;
+            Wheat: string;
+            Fish: string;
+          };
+          halal: string;
+          vegan: string;
+          vegetarian: string;
+        }
+      ];
+    }[];
+    cosmetics: {
+      name: string;
+      models: [
+        {
+          name: string;
+          alcohol: string;
+          allergic: {
+            Gluten: string;
+            Milk: string;
+            Egg: string;
+            Nuts: string;
+            Sesame: string;
+            Wheat: string;
+            Fish: string;
+          };
+          halal: string;
+          vegan: string;
+          vegetarian: string;
+        }
+      ];
+    }[];
+  };
+}
+function ProductItem() {
+  const [productItems, setProductItems] = useState<Category[]>([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:3001/categories/category"
+        );
+        setProductItems(response.data.category.drink);
+      } catch (error) {
+        console.log(error);
+      }
+      getProducts();
+    };
+  }, []);
+  return <div>hallo</div>;
+}
+
+export default ProductItem;
