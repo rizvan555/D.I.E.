4380375083bd30a2d7188b054688b529d6e4d14a
Rizvan <rizvan@MacBook-Air.fritz.box>
2023-07-13 12:48:35 +0200

    created new page with addProduct function

--- pages/addProduct/index.tsx
+++ pages/addProduct/index.tsx
@@ -1,113 +1,90 @@
-import { Checkbox, InputLabel } from '@mui/material';
-import axios from 'axios';
 import { NextPage } from 'next';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 const AddProduct: NextPage = () => {
-  const [categoryName, setCategoryName] = useState('');
-  const [productName, setProductName] = useState('');
-  const [modelName, setModelName] = useState('');
-  const [inputValue, setInputValue] = useState('');
-  const [mainCheckBoxes, setMainCheckBoxes] = useState([
-    { name: 'Halal', status: true },
-    { name: 'Vegan', status: true },
-    { name: 'Vegetarian', status: true },
-    { name: 'Alcohol', status: true },
+  const [inputs, setInputs] = useState([
+    { title: 'Category', type: 'text', placeholder: 'Category' },
+    { title: 'Product', type: 'text', placeholder: 'Product' },
+    { title: 'Model', type: 'text', placeholder: 'Model' },
   ]);
-  const [allergicCheckBoxes, setAllergicCheckBoxes] = useState([
-    { name: 'Gluten', status: true },
-    { name: 'Milk', status: true },
-    { name: 'Egg', status: true },
-    { name: 'Nuts', status: true },
-    { name: 'Sesame', status: true },
-    { name: 'Wheat', status: true },
-    { name: 'Fish', status: true },
-  ]);
-  const [results, setResults] = useState<string[]>([]);
-
-  const handleCategorySubmit = async (e: any) => {
+  const [categoryValue, setCategoryValue] = useState('');
+  const [productValue, setProductValue] = useState('');
+  const [modelValue, setModelValue] = useState('');
+  const [submittedValuesCategory, setSubmittedValuesCategory] = useState({
+    category: '',
+  });
+  const [submittedValuesProduct, setSubmittedValuesProduct] = useState({
+    product: '',
+  });
+  const [submittedValuesModel, setSubmittedValuesModel] = useState({
+    model: '',
+  });
+  const handleSubmit = (e: any) => {
     e.preventDefault();
-    try {
-      const response = await axios.post('/categories/category', {
-        category: {
-          drink: [],
-        },
+    if (categoryValue) {
+      setSubmittedValuesCategory({
+        category: categoryValue,
       });
-      console.log(response.data);
-    } catch (error) {
-      console.log(error);
+      setCategoryValue('');
     }
-  };
-
-  const addProduct = () => {
-    if (inputValue) {
-      const newResults = [...results, inputValue];
-      setResults(newResults);
+    if (productValue) {
+      setSubmittedValuesProduct({
+        product: productValue,
+      });
+      setProductValue('');
     }
-    setInputValue('')
+    if (modelValue) {
+      setSubmittedValuesModel({
+        model: modelValue,
+      });
+      setModelValue('');
+    }
   };
 
   return (
-    <div className="flex justify-center mt-10 h-[100vh]">
-      <form className="flex flex-col gap-3" onSubmit={handleCategorySubmit}>
-        <label className="flex gap-2">
-          <h3 className="w-[10vw]">Category:</h3>
-          <input
-            type="text"
-            value={categoryName}
-            onChange={(e) => setCategoryName(e.target.value)}
-            className="border w-[20vw]"
-          />
-        </label>
-        <label className="flex gap-2">
-          <h3 className="w-[10vw]">Product:</h3>
-          <input
-            type="text"
-            value={productName}
-            onChange={(e) => setProductName(e.target.value)}
-            className="border w-[20vw]"
-          />
-        </label>
-        <label className="flex gap-2">
-          <h3 className="w-[10vw]">Model:</h3>
-          <input
-            type="text"
-            value={modelName}
-            onChange={(e) => setModelName(e.target.value)}
-            className="border w-[20vw]"
-          />
-        </label>
-        <div className="">
-          <button className="border px-4 py2" onClick={addProduct}>
-            Send
-          </button>
-        </div>
-
-        <div className="flex gap-5 flex-col">
-          <div className="flex gap-10">
-            {mainCheckBoxes.map((mainCheckBox, index) => (
-              <div className="flex items-center" key={index}>
-                <h3>{mainCheckBox.name}</h3>
-                <Checkbox />
-              </div>
-            ))}
+    <div>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-5 justify-center items-center m-20"
+      >
+        {inputs.map((input, index) => (
+          <div className="" key={index}>
+            {input.title === 'Category' && (
+              <input
+                value={categoryValue}
+                onChange={(e) => setCategoryValue(e.target.value)}
+                type={input.type}
+                placeholder={input.placeholder}
+                className="border"
+              />
+            )}
+            {input.title === 'Product' && (
+              <input
+                value={productValue}
+                onChange={(e) => setProductValue(e.target.value)}
+                type={input.type}
+                placeholder={input.placeholder}
+                className="border"
+              />
+            )}
+            {input.title === 'Model' && (
+              <input
+                value={modelValue}
+                onChange={(e) => setModelValue(e.target.value)}
+                type={input.type}
+                placeholder={input.placeholder}
+                className="border"
+              />
+            )}
           </div>
-
-          <div className="flex gap-8 w-[50vw]">
-            {allergicCheckBoxes.map((allergicCheckBox, index) => (
-              <div className="flex items-center " key={index}>
-                <h4>{allergicCheckBox.name}</h4>
-                <Checkbox />
-              </div>
-            ))}
-          </div>
-        </div>
+        ))}
+        <button className="border" type="submit">
+          SEND
+        </button>
       </form>
-      <div className="flex flex-col">
-        <div>Category: {categoryName}</div>
-        <div>Product: {productName}</div>
-        <div>Model: {modelName}</div>
-      </div>
+      <div>Category: {submittedValuesCategory.category}</div>
+      <div>Product: {submittedValuesProduct.product}</div>
+      <div>Model: {submittedValuesModel.model}</div>
     </div>
   );
 };
